52bf73bfcd188dac4a52f45e849100823e467c73
Diego <dede2040a@gmail.com>
2025-04-28 10:00:13 -0300

    feat: cria e organiza os projetos

---
+++ src/projects.js
@@ -0,0 +1,27 @@
+function createProject(name) {
+    let todos = [];
+    const addNewTodo = (todo) => {
+        todos.push(todo);
+    } 
+    const getTodos = () => {
+        return todos;
+    };
+
+    const getTodoByName = (name) => {
+        return todos[name];
+    };
+
+    const setTodoByIndex = (index, todo) => {
+        todos[index] = todo;
+    };
+
+    return {
+        name,
+        addNewTodo,
+        getTodos,
+        getTodoByName,
+        setTodoByIndex,
+    };
+}
+
+export default createProject;
